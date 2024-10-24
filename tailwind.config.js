16ef6c6b9f65fdb2dc8fe8e5b5e5b7ba92fe97cf
mohamed ali ezzeddine <mohamedaliezzeddine1@gmail.com>
2024-10-24 23:08:17 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
+
